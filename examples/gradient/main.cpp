c988114354255747bd35c6b70563e416f984b810
Jonas Rembser <jonas.rembser@cern.ch>
2024-03-23 21:30:11 +0100

    Use two-point gradient in Newton example

--- examples/gradient/main.cpp
+++ examples/gradient/main.cpp
@@ -23,12 +23,36 @@ struct GradientValue {
 
 using Function = std::function<double(std::span<const double>)>;
 
-GradientValue update_gradient(Function func, std::span<const double> params, GradientValue prev)
+constexpr double eps_machine = 4. * std::numeric_limits<double>::epsilon();
+constexpr double eps_machine_2 = 2. * std::sqrt(eps_machine);
+
+GradientValue update_gradient(Function func, std::span<const double> params, std::span<const double> param_errors)
 {
    std::vector<double> xVals{params.begin(), params.end()};
    const double centralVal = func(params);
 
+   GradientValue out;
+   out.grad.resize(params.size());
+   out.g2.resize(params.size());
+   out.step.resize(params.size());
+
    for (unsigned int i = 0; i < params.size(); i++) {
+
+      double error_def = 1.0;
+
+
+      // TODO: when we support parameters with limits, this logic needs to be
+      // updated such that the errors are clipped by the limits if the
+      // parameter is near it (see InitialGradientCalculator).
+      double vplu = param_errors[i];
+      double vmin = param_errors[i];
+
+      double gsmin = 8. * eps_machine_2 * (std::abs(xVals[i]) + eps_machine_2);
+      // protect against very small step sizes which can cause dirin to zero and then nan values in grd
+      double dirin = std::max(0.5 * (std::abs(vplu) + std::abs(vmin)), gsmin);
+      double g2 = 2.0 * error_def / (dirin * dirin);
+      double gstep = std::max(gsmin, 0.1 * dirin);
+      double grd = g2 * dirin;
 
       auto funcForParam = [&](double x) -> double {
          if (xVals[i] == x) {
@@ -41,23 +65,24 @@ GradientValue update_gradient(Function func, std::span<const double> params, Gra
          return out;
       };
 
-      mnfs::DerivativeState state{prev.grad[i], prev.g2[i], prev.step[i]};
+      mnfs::DerivativeState state{grd, g2, gstep};
       state = mnfs::update_derivative(state, funcForParam, params[i]);
-      prev.grad[i] = state.grad;
-      prev.g2[i] = state.g2;
-      prev.step[i] = state.step;
+      out.grad[i] = state.grad;
+      out.g2[i] = state.g2;
+      out.step[i] = state.step;
    }
 
-   return prev;
+   return out;
 }
 
 int main(int, char *[])
 {
    std::vector<double> params{2.0, 3.0};
+   std::vector<double> param_errors{0.1, 0.1};
 
-   GradientValue grad{{20., 20.}, {200., 200.}, {0.01, 0.01}};
+   //GradientValue grad{{20., 20.}, {200., 200.}, {0.01, 0.01}};
 
-   grad = update_gradient(func, params, grad);
+   GradientValue grad = update_gradient(func, params, param_errors);
 
    fmt::println("{}", grad.grad);
    fmt::println("{}", grad.g2);
--- examples/newton/main.cpp
+++ examples/newton/main.cpp
@@ -1,3 +1,5 @@
+#include <mnfs/derivative.h>
+
 #include <iostream>
 #include <armadillo>
 #include <cmath>
@@ -16,105 +18,163 @@ using Gradient = std::function<void(std::span<double>, std::span<const double>)>
 using Hessian = std::function<void(std::span<double>, std::span<const double>)>;
 
 constexpr double eps = 1e-5;
-constexpr double eps_inv = 1/1e-5;
+constexpr double eps_inv = 1 / 1e-5;
 
-auto num_grad(Function func) -> Gradient {
-    return [=](std::span<double> out, std::span<const double> params) -> void {
+constexpr double eps_machine = 4. * std::numeric_limits<double>::epsilon();
+constexpr double eps_machine_2 = 2. * std::sqrt(eps_machine);
 
-        std::vector<double> tmp(params.begin(), params.end());
-        for (std::size_t i = 0; i < params.size(); ++i) {
-            double param = params[i];
-            tmp[i] = param + eps;
-            double upper = func(tmp);
-            tmp[i] = param - eps;
-            double lower = func(tmp);
-            tmp[i] = param;
-            out[i] = 0.5 * (upper - lower) * eps_inv;
-        }
-    };
+void get_gradient(std::span<double> out, Function func, std::span<const double> params,
+                  std::span<const double> param_errors)
+{
+   std::vector<double> xVals{params.begin(), params.end()};
+   const double centralVal = func(params);
+
+   struct GradientValue {
+      std::vector<double> grad;
+      std::vector<double> g2;
+      std::vector<double> step;
+   };
+
+   GradientValue info;
+   info.grad.resize(params.size());
+   info.g2.resize(params.size());
+   info.step.resize(params.size());
+
+   for (unsigned int i = 0; i < params.size(); i++) {
+
+      double error_def = 1.0;
+
+      // TODO: when we support parameters with limits, this logic needs to be
+      // updated such that the errors are clipped by the limits if the
+      // parameter is near it (see InitialGradientCalculator).
+      double vplu = param_errors[i];
+      double vmin = param_errors[i];
+
+      double gsmin = 8. * eps_machine_2 * (std::abs(xVals[i]) + eps_machine_2);
+      // protect against very small step sizes which can cause dirin to zero and then nan values in grd
+      double dirin = std::max(0.5 * (std::abs(vplu) + std::abs(vmin)), gsmin);
+      double g2 = 2.0 * error_def / (dirin * dirin);
+      double gstep = std::max(gsmin, 0.1 * dirin);
+      double grd = g2 * dirin;
+
+      auto funcForParam = [&](double x) -> double {
+         if (xVals[i] == x) {
+            return centralVal;
+         }
+         double tmp = xVals[i];
+         xVals[i] = x;
+         const double out = func(xVals);
+         xVals[i] = tmp;
+         return out;
+      };
+
+      mnfs::DerivativeState state{grd, g2, gstep};
+      state = mnfs::update_derivative(state, funcForParam, params[i]);
+      info.grad[i] = state.grad;
+      info.g2[i] = state.g2;
+      info.step[i] = state.step;
+   }
+
+   for (std::size_t i = 0; i < params.size(); ++i) {
+      out[i] = info.grad[i];
+   }
 }
 
-auto num_hess(Gradient grad) -> Hessian {
-    return [=](std::span<double> out, std::span<const double> params) -> void {
-        std::vector<double> tmp(params.begin(), params.end());
-        std::vector<double> grad_upper(params.size());
-        std::vector<double> grad_lower(params.size());
-        for (std::size_t i = 0; i < params.size(); ++i) {
-            double param = params[i];
-            tmp[i] = param + eps;
-            grad(grad_upper, tmp);
-            tmp[i] = param - eps;
-            grad(grad_lower, tmp);
-            tmp[i] = param;
-            for (std::size_t j = 0; j < params.size(); ++j) {
-                out[params.size() * i + j] = 0.5 * (grad_upper[j] - grad_lower[j]) * eps_inv;
-            }
-        }
-    };
+auto num_grad(Function func, std::span<const double> param_errors) -> Gradient
+{
+   return [=](std::span<double> out, std::span<const double> params) -> void {
+      get_gradient(out, func, params, param_errors);
+   };
 }
 
-auto edm(Function func, std::span<const double> params) -> double {
-    const auto func_grad = num_grad(func);
-    const auto func_hess = num_hess(func_grad);
-
-    std::vector<double> grad(params.size());
-    std::vector<double> hess(params.size() * params.size());
-
-    func_grad(grad, params);
-    func_hess(hess, params);
-
-    arma::vec params_vec{params.data(), params.size()};
-    arma::vec grad_vec{grad.data(), params.size(), false};
-    arma::mat hess_mat{hess.data(), params.size(), params.size(), false};
-
-    return arma::mat{0.5 * grad_vec.t() * arma::inv(hess_mat) * grad_vec}(0, 0);
+auto num_hess(Gradient grad) -> Hessian
+{
+   return [=](std::span<double> out, std::span<const double> params) -> void {
+      std::vector<double> tmp(params.begin(), params.end());
+      std::vector<double> grad_upper(params.size());
+      std::vector<double> grad_lower(params.size());
+      for (std::size_t i = 0; i < params.size(); ++i) {
+         double param = params[i];
+         tmp[i] = param + eps;
+         grad(grad_upper, tmp);
+         tmp[i] = param - eps;
+         grad(grad_lower, tmp);
+         tmp[i] = param;
+         for (std::size_t j = 0; j < params.size(); ++j) {
+            out[params.size() * i + j] = 0.5 * (grad_upper[j] - grad_lower[j]) * eps_inv;
+         }
+      }
+   };
 }
 
-auto newton_stepper(Function func, double gamma) -> std::function<void(std::span<double> out, std::span<const double> params)> {
-    const auto func_grad = num_grad(func);
-    const auto func_hess = num_hess(func_grad);
-    return [=](std::span<double> out, std::span<const double> params) -> void {
-        std::vector<double> grad(params.size());
-        std::vector<double> hess(params.size() * params.size());
+auto edm(Function func, Gradient func_grad, std::span<const double> params) -> double
+{
+   const auto func_hess = num_hess(func_grad);
 
-        func_grad(grad, params);
-        func_hess(hess, params);
+   std::vector<double> grad(params.size());
+   std::vector<double> hess(params.size() * params.size());
 
-        arma::vec params_vec{params.data(), params.size()};
-        arma::vec out_vec{out.data(), out.size(), false};
-        arma::vec grad_vec{grad.data(), params.size(), false};
-        arma::mat hess_mat{hess.data(), params.size(), params.size(), false};
+   func_grad(grad, params);
+   func_hess(hess, params);
 
-        arma::vec rhs = hess_mat * params_vec - gamma * grad_vec;
+   arma::vec params_vec{params.data(), params.size()};
+   arma::vec grad_vec{grad.data(), params.size(), false};
+   arma::mat hess_mat{hess.data(), params.size(), params.size(), false};
 
-        solve(out_vec, hess_mat, rhs);
-    };
+   return arma::mat{0.5 * grad_vec.t() * arma::inv(hess_mat) * grad_vec}(0, 0);
+}
+
+auto newton_stepper(Function func, Gradient func_grad, double gamma)
+   -> std::function<void(std::span<double> out, std::span<const double> params)>
+{
+   const auto func_hess = num_hess(func_grad);
+   return [=](std::span<double> out, std::span<const double> params) -> void {
+      std::vector<double> grad(params.size());
+      std::vector<double> hess(params.size() * params.size());
+
+      func_grad(grad, params);
+      func_hess(hess, params);
+
+      arma::vec params_vec{params.data(), params.size()};
+      arma::vec out_vec{out.data(), out.size(), false};
+      arma::vec grad_vec{grad.data(), params.size(), false};
+      arma::mat hess_mat{hess.data(), params.size(), params.size(), false};
+
+      arma::vec rhs = hess_mat * params_vec - gamma * grad_vec;
+
+      solve(out_vec, hess_mat, rhs);
+   };
 }
 
 } // namespace mnfs
 
-double func(std::span<const double> params) {
-    const double x = params[0];
-    const double y = params[1];
-    return x * x * x * x + y * y * y * y;
+double func(std::span<const double> params)
+{
+   const double x = params[0];
+   const double y = params[1];
+   return x * x * x * x + y * y * y * y;
 }
 
 int main()
 {
-  std::vector<double> params{2.0, 3.0};
+   std::vector<double> params{2.0, 3.0};
+   std::vector<double> param_errors{0.1, 0.1};
 
-  auto newton_step = mnfs::newton_stepper(func, 1.0);
+   const auto func_grad = mnfs::num_grad(func, param_errors);
 
-  double edm = mnfs::edm(func, params);
+   auto newton_step = mnfs::newton_stepper(func, func_grad, 1.0);
 
-  fmt::println("init  : {}", params);
-  for (int i = 0; i < 20; ++i) {
-     newton_step(params, params);
-     fmt::println("step {}: {}", i, params);
-     edm = mnfs::edm(func, params);
-     if (edm < 1e-12) break;
-  }
-  fmt::println("fval: {}", func(params));
+   double edm = mnfs::edm(func, func_grad, params);
 
-  return 0;
+   fmt::println("init  : {}", params);
+   for (int i = 0; i < 20; ++i) {
+      newton_step(params, params);
+      fmt::println("step {}: {}", i, params);
+      edm = mnfs::edm(func, func_grad, params);
+      if (edm < 1e-12)
+         break;
+   }
+   fmt::println("fval: {}", func(params));
+
+   return 0;
 }
